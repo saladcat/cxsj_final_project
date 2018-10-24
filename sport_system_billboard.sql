7e528ccfd45c64ce8a7a0b2674177729074f7546
saladcat <djytyang@gmail.com>
2018-10-24 12:11:14 +0800

    add the sql_file

---
+++ sport_system_billboard.sql
@@ -0,0 +1,134 @@
+CREATE TABLE sport_system.billboard
+(
+    ad_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    ad_name varchar(100),
+    ad_content varchar(100),
+    ad_data_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    is_delete int(11) DEFAULT '0'
+);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (1, 'ds', 'sd', '2018-10-23 05:06:13', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (2, 'dsa', 'dsa', '2018-10-23 05:23:06', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (3, null, null, '2018-10-23 14:51:47', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (4, 'dsdsad', 'dsada', '2018-10-23 14:52:06', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (5, null, null, '2018-10-23 14:53:09', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (6, null, null, '2018-10-23 14:53:37', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (7, null, null, '2018-10-23 14:54:18', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (8, null, null, '2018-10-23 14:54:39', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (9, null, null, '2018-10-23 14:55:16', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (10, null, null, '2018-10-23 14:55:41', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (11, null, null, '2018-10-23 14:56:19', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (12, null, null, '2018-10-23 14:56:24', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (13, null, null, '2018-10-23 14:56:52', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (14, null, null, '2018-10-23 14:57:52', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (15, null, null, '2018-10-23 14:58:11', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (16, null, null, '2018-10-23 14:59:05', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (17, null, null, '2018-10-23 14:59:32', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (18, null, null, '2018-10-23 14:59:51', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (19, null, null, '2018-10-23 15:00:34', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (20, null, null, '2018-10-23 15:01:06', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (21, null, null, '2018-10-23 15:01:36', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (22, null, null, '2018-10-23 15:02:59', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (23, null, null, '2018-10-23 15:03:13', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (24, null, null, '2018-10-23 15:03:18', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (25, null, null, '2018-10-24 10:34:01', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (26, null, null, '2018-10-24 10:34:37', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (27, null, null, '2018-10-24 10:35:44', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (28, null, null, '2018-10-24 10:36:11', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (29, null, null, '2018-10-24 10:37:06', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (30, null, null, '2018-10-24 10:37:21', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (31, null, null, '2018-10-24 10:37:32', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (32, null, null, '2018-10-24 10:38:33', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (33, null, null, '2018-10-24 10:39:41', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (34, null, null, '2018-10-24 10:40:36', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (35, null, null, '2018-10-24 10:42:05', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (36, null, null, '2018-10-24 10:43:12', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (37, null, null, '2018-10-24 10:44:15', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (38, null, null, '2018-10-24 10:44:29', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (39, null, null, '2018-10-24 10:44:51', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (40, null, null, '2018-10-24 10:45:20', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (41, null, null, '2018-10-24 10:47:44', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (42, null, null, '2018-10-24 10:48:11', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (43, null, null, '2018-10-24 10:49:14', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (44, null, null, '2018-10-24 10:50:41', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (45, null, null, '2018-10-24 10:52:52', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (46, null, null, '2018-10-24 10:56:15', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (47, 'title1', 'content1', '2018-10-24 10:57:28', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (48, 'title1', 'content1', '2018-10-24 10:59:07', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (49, null, null, '2018-10-24 11:00:19', 0);
+INSERT INTO sport_system.billboard (ad_id, ad_name, ad_content, ad_data_time, is_delete) VALUES (50, null, null, '2018-10-24 11:01:18', 0);
+CREATE TABLE sport_system.department
+(
+    department_id int(11) PRIMARY KEY NOT NULL,
+    grade int(11) NOT NULL,
+    class int(11) NOT NULL,
+    department_name varchar(50),
+    is_delete int(11) DEFAULT '0'
+);
+CREATE TABLE sport_system.event
+(
+    event_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    event_name varchar(30),
+    max_team_member int(11),
+    min_team_member int(11),
+    team_limit int(11),
+    year int(11),
+    is_delete int(11) DEFAULT '0'
+);
+INSERT INTO sport_system.event (event_id, event_name, max_team_member, min_team_member, team_limit, year, is_delete) VALUES (1, 'ft', null, null, null, null, 0);
+CREATE TABLE sport_system.`match`
+(
+    match_id int(11) PRIMARY KEY NOT NULL,
+    `order` int(11),
+    score int(11),
+    is_finished int(11),
+    vs_time int(11),
+    event_id int(11),
+    team_id int(11),
+    is_delete int(11) DEFAULT '0'
+);
+CREATE TABLE sport_system.passwd
+(
+    user_id int(11) PRIMARY KEY NOT NULL,
+    password varchar(100),
+    is_delete int(11) DEFAULT '0'
+);
+CREATE UNIQUE INDEX passwd_user_id_uindex ON sport_system.passwd (user_id);
+INSERT INTO sport_system.passwd (user_id, password, is_delete) VALUES (20165083, '1998', 0);
+CREATE TABLE sport_system.registration
+(
+    reg_id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    team_id int(11),
+    event_id int(11),
+    create_time timestamp DEFAULT CURRENT_TIMESTAMP,
+    is_delete int(11) DEFAULT '0'
+);
+INSERT INTO sport_system.registration (reg_id, team_id, event_id, create_time, is_delete) VALUES (1, 1, 1, null, 0);
+INSERT INTO sport_system.registration (reg_id, team_id, event_id, create_time, is_delete) VALUES (2, 2, 1, null, 0);
+CREATE TABLE sport_system.team
+(
+    team_id int(11) NOT NULL,
+    user_id int(11) NOT NULL,
+    team_name varchar(50),
+    is_delete int(11) DEFAULT '0',
+    CONSTRAINT `PRIMARY` PRIMARY KEY (team_id, user_id)
+);
+INSERT INTO sport_system.team (team_id, user_id, team_name, is_delete) VALUES (1, 1, 't1', 0);
+INSERT INTO sport_system.team (team_id, user_id, team_name, is_delete) VALUES (1, 2, 't1', 0);
+INSERT INTO sport_system.team (team_id, user_id, team_name, is_delete) VALUES (2, 3, 't2', 0);
+INSERT INTO sport_system.team (team_id, user_id, team_name, is_delete) VALUES (3, 4, 't2', 0);
+CREATE TABLE sport_system.user
+(
+    user_id int(11) PRIMARY KEY NOT NULL,
+    department_id int(11),
+    phone varchar(20),
+    gender int(11),
+    email varchar(50),
+    name varchar(20),
+    is_delete int(11) DEFAULT '0',
+    is_admin int(11) DEFAULT '0',
+    password varchar(100)
+);
+INSERT INTO sport_system.user (user_id, department_id, phone, gender, email, name, is_delete, is_admin, password) VALUES (1, 1, '1', 1, 'a215900031@gmail', 'zsy1', 0, 0, null);
+INSERT INTO sport_system.user (user_id, department_id, phone, gender, email, name, is_delete, is_admin, password) VALUES (2, null, null, null, null, 'zsy2', 0, 0, null);
+INSERT INTO sport_system.user (user_id, department_id, phone, gender, email, name, is_delete, is_admin, password) VALUES (3, null, null, null, null, 'zsy3', 0, 0, null);
+INSERT INTO sport_system.user (user_id, department_id, phone, gender, email, name, is_delete, is_admin, password) VALUES (4, null, null, null, null, 'zsy4', 0, 0, null);
